51ad63f2af1dbf9ff86d5d47d2d8ed6b1974fefc
mfannick <mfannick1@gmail.com>
2019-09-15 14:16:41 +0200

    initializing the app

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+#initializing the news app
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+bootstrap = Bootstrap()
+def createApp(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+      # setting config
+    from .requests import config_request
+    config_request(app)
+  
+
+
+
+    return app
