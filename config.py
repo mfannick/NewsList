45e04aa8a3b7b1cad9f0a785eb2845aebcc32b65
mfannick <mfannick1@gmail.com>
2019-09-15 14:34:30 +0200

    for our app configuration

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_WEB_URL='https://newsapi.org/v2/sources?/category={}&apiKey={}'
+    ARTICLES_URL='https://newsapi.org/v2/everything?id={}&apiKey={}'
+    NEWS_API_KEY='2d46df7bd2ef45689ca30c90eadf3061'
+    # NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    pass
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
