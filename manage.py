de68bbf72ca93e8175bd0f320efb28921d96277e
mfannick <mfannick1@gmail.com>
2019-09-15 14:34:56 +0200

    for running our app

---
+++ manage.py
@@ -0,0 +1,12 @@
+from app import createApp
+from flask_script import Manager,Server
+
+# Creating app instance
+app = createApp('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+
+if __name__ == '__main__':
+    manager.run()
